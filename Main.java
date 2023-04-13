2a8f03666d903448de847e9dae149d2d920c5431
Farjana_hasan <107532082+farjana806@users.noreply.github.com>
2023-04-14 00:09:42 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
+
+public class Main
+{
+    public static void main(String args[])
+    {
         System.out.println("Enter the BIll Number: ");
         Scanner sc = new Scanner(System.in);
         String bill = sc.next();
         System.out.println("Enter the password: ");
         String pass = sc.next();
-        Bill b = new Bill();
+        Bill b = new Bill();//bill object
         b.setBillnumber("21335806");
         b.setPass("farjana");
         String x = b.getBillnumber();
@@ -16,7 +19,9 @@ public class Main {
         if(a==0 && c==0)
         {
             b.logindisplay();
-
+            b.input_data();
+            b.calculate_bill();
+            b.display();
         }
         else
         {
